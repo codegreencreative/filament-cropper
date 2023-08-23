376a9df78fed44e44e9283df154ea06bbce8e56d
Jesse Vista <jvista@upwebdesign.com>
2023-08-23 16:43:47 -0400

    Use spatie PackageServiceProvider
    Signed-off-by: Jesse Vista <jvista@upwebdesign.com>

--- src/FilamentCropperServiceProvider.php
+++ src/FilamentCropperServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace Nuhel\FilamentCropper;
 
-use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentCropperServiceProvider extends PluginServiceProvider
+class FilamentCropperServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-cropper';
 
